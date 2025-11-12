a8ce72e42957d6b5e7a0e80c388d80618cf31431
composable-tu <composable@foxmail.com>
2025-11-12 18:26:20 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import {createApp} from 'vue'
 import App from './App.vue'
-import router from "@/router/index.ts";
+import router from "@/router/index";
 import {createPinia} from "pinia";
 
 const pinia = createPinia()
